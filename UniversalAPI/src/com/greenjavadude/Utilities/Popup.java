cdc2c4103282c37ceb80faf6b156e75d4be1e653
Green Java Dude <greenjavadude@gmail.com>
2015-07-13 22:05:28 +0200

    Popup.java

---
+++ UniversalAPI/src/com/greenjavadude/Utilities/Popup.java
@@ -0,0 +1,13 @@
+package com.greenjavadude.Utilities;
+
+import javax.swing.JOptionPane;
+
+public class Popup {
+	public String askQuestion(String question){
+		return JOptionPane.showInputDialog(null, question, "Answer Question", JOptionPane.QUESTION_MESSAGE);
+	}
+	
+	public void inform(String message){
+		JOptionPane.showMessageDialog(null, message);
+	}
+}
